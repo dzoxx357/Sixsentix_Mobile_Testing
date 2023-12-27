819480aa3bd7ec76d188e83a9b4ed7d16b2bb7e4
Djordje Popov <popov.djordje@gmail.com>
2023-12-27 12:46:50 +0100

    some reworks

--- src/main/java/Runners/Run.java
+++ src/main/java/Runners/Run.java
@@ -3,8 +3,9 @@ package Runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
+
 @CucumberOptions(
-        features = "src/test/resources/FirstTest.feature",
+        features = "src/test/resources/SecondTest.feature",
         glue = {"StepDefinitions"}
 )
 
