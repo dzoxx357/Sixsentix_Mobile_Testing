8bf879e7a311eb42f886ed02656251af6702d80e
Djordje Popov <popov.djordje@gmail.com>
2023-12-29 08:54:20 +0100

    added functionalities for reporting and cucumber steps

--- src/main/java/Runners/Run.java
+++ src/main/java/Runners/Run.java
@@ -6,7 +6,8 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         features = "src/test/resources/SecondTest.feature",
-        glue = {"StepDefinitions"}
+        glue = {"StepDefinitions"},
+        plugin = {"pretty", "html:target/cucumber-reports"}
 )
 
 public class Run extends AbstractTestNGCucumberTests {
